be43d49ed5083a8ae6e1147f2b7515127a2a3232
rmontroy <rob.montroy@vai.org>
2020-09-03 13:54:59 -0400

    status filter mockup; replace rmwc w/ M-UI

--- src/js/SlideTable.js
+++ src/js/SlideTable.js
@@ -1,17 +1,28 @@
 import { html } from 'htm/react';
-import {
-  DataTableCell,
-  DataTableHeadCell,
-  DataTableRow,
-  DataTableBody,
-  DataTableContent,
-  DataTableHead,
-} from '@rmwc/data-table'
-import { LinearProgress } from '@rmwc/linear-progress'
+import { makeStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Chip from '@material-ui/core/Chip'
 import '@material/data-table/dist/mdc.data-table.css'
 import '@rmwc/data-table/data-table.css'
-import '@material/linear-progress/dist/mdc.linear-progress.css'
 import { useTable } from 'react-table'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(0.5),
+    },
+  },
+}));
 
 function SlideTable({
   columns,
@@ -27,49 +38,57 @@ function SlideTable({
     rows, 
     prepareRow,
     visibleColumns,
-  } = useTable({ columns, data },
-  )
-
+  } = useTable({ columns, data })
+  const classes = useStyles();
 
   return html`
-    <${DataTableContent} ...${getTableProps()}>
-      <${DataTableHead}>
-        ${headerGroups.map(headerGroup => (html`
-          <${DataTableRow} ...${headerGroup.getHeaderGroupProps()}>
-            ${headerGroup.headers.map(column => (html`
-              <${DataTableHeadCell} ...${column.getHeaderProps(getHeaderProps(column))}>
-                ${column.render('Header')}
-              </${DataTableHeadCell}>
-            `))}
-          </${DataTableRow}>
-        `))}
-      </${DataTableHead}>
-      ${loading && html`
-        <${DataTableHead}>
-          <${DataTableRow}>
-            <${DataTableHeadCell} colSpan=${visibleColumns.length} style=${{ height: 1, padding: 0 }}>
+    <${TableContainer} component=${Paper}>
+    <${Table} ...${getTableProps()}>
+      <${TableHead}>
+        <${TableRow}>
+          <${TableCell} colSpan=${visibleColumns.length}>
+            <div className=${classes.root}>
+              <${Chip} color="primary" label="QC Inspection" clickable />
+              <${Chip} variant="outlined" label="Pathology Review" clickable />
+              <${Chip} variant="outlined" label="Case Search" clickable />
+            </div>
+          </${TableCell}>
+        </${TableRow}>
+        ${loading && html`
+          <${TableRow}>
+            <${TableCell} colSpan=${visibleColumns.length} style=${{ height: 1, padding: 0 }}>
               <${LinearProgress} />
-            </${DataTableHeadCell}>
-          </${DataTableRow}>
-        </${DataTableHead}>
-      `}
-      <${DataTableBody}>
+            </${TableCell}>
+          </${TableRow}>
+        `}
+        ${headerGroups.map(headerGroup => (html`
+          <${TableRow} ...${headerGroup.getHeaderGroupProps()}>
+            ${headerGroup.headers.map(column => (html`
+              <${TableCell} ...${column.getHeaderProps(getHeaderProps(column))}>
+                ${column.render('Header')}
+              </${TableCell}>
+            `))}
+          </${TableRow}>
+        `))}
+      </${TableHead}>
+      <${TableBody}>
         ${rows.map((row, i) => {
           prepareRow(row)
           return html`
-            <${DataTableRow} ...${row.getRowProps(getRowProps(row))}>
+            <${TableRow} ...${row.getRowProps(getRowProps(row))}>
               ${row.cells.map(cell => {
                 return html`
-                  <${DataTableCell} ...${cell.getCellProps()}>
+                  <${TableCell} ...${cell.getCellProps()}>
                     ${cell.render('Cell')}
-                  </${DataTableCell}>
+                  </${TableCell}>
                 `
               })}
-            </${DataTableRow}>
+            </${TableRow}>
           `
         })}
-      </${DataTableBody}>
-    </${DataTableContent}>
+      </${TableBody}>
+      </${Table}>
+    </${TableContainer}>
   `
 }
 
