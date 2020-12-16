rmontroy/virtual-slide-viewer

commit c267a8137be680fc5b8da8918b9ccc64e73a2400

#4

src/DataView/js/DataView.js
@@ -17,6 +17,16 @@ const client = new ApolloClient({
     typePolicies: {
       Slide: {
         keyFields: ["ImageID"]
+      },
+      Query: {
+        fields: {
+          feed: {
+            keyArgs: ["Status"],
+            merge(existing = [], incoming) {
+              return [...existing, ...incoming];
+            },
+          }
+        }
       }
     }
   }),
@@ -96,7 +106,7 @@ const COLUMNS =
 function DataView() {
   const [statusFilter, setStatusFilter] = useState(Statuses[0]);
   const [casesFilter, setCasesFilter] = useState([]);
-  const QueryByStatus = useQuery(GET_SLIDES_BY_STATUS, {client, variables: { statusFilter }});
+  const QueryByStatus = useQuery(GET_SLIDES_BY_STATUS, {client, variables: { statusFilter, limit: 5 }});
   const [getCaseData, QueryByCase] = useLazyQuery(GET_SLIDES, {client, variables: { ImageIDs: casesFilter }});
   const [updateSlideID] = useMutation(UPDATE_SLIDEID, {client});
   const [updateCaseID] = useMutation(UPDATE_CASEID, {client});
@@ -168,6 +178,12 @@ function DataView() {
     });
   }
 
+  const fetchMore = () => QueryByStatus.fetchMore({
+    variables: {
+      nextToken: QueryByStatus.data.Slides.nextToken,
+    },
+  })
+
   return html`
     <${ApolloProvider} client=${client}>
       <div>
@@ -189,6 +205,7 @@ function DataView() {
           loading=${currentQuery.loading}
           selectionChanged=${selectionChanged}
           updateField=${updateField}
+          fetchMore=${byStatus ? fetchMore : false}
         />
         <${Snackbar}
           anchorOrigin=${{

src/DataView/js/DataTable.js
@@ -1,4 +1,5 @@
 import { html } from 'htm/react';
+import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -8,17 +9,29 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Checkbox from '@material-ui/core/Checkbox';
+import Chip from '@material-ui/core/Chip';
 import { useTable, useRowSelect } from 'react-table';
 import { useEffect } from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  fetchMore: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    margin: theme.spacing(3),
+  }
+}));
 
 function DataTable({
   columns,
   data,
   loading,
   selectionChanged,
-  updateField
+  updateField,
+  fetchMore
 })
 {
+  const classes = useStyles();
   const { 
     getTableProps, 
     headerGroups, 
@@ -66,40 +79,50 @@ function DataTable({
     <${TableContainer} component=${Paper}>
       <${Table} ...${getTableProps()}>
         <${TableHead}>
-        ${loading && html`
-          <${TableRow}>
-            <${TableCell} colSpan=${visibleColumns.length} style=${{ height: 1, padding: 0 }}>
-              <${LinearProgress} />
-            </${TableCell}>
-          </${TableRow}>
-        `}
-        ${headerGroups.map(headerGroup => (html`
-          <${TableRow} ...${headerGroup.getHeaderGroupProps()}>
-            ${headerGroup.headers.map(column => (html`
-              <${TableCell} ...${column.getHeaderProps()}>
-                ${column.render('Header')}
+          ${loading && html`
+            <${TableRow}>
+              <${TableCell} colSpan=${visibleColumns.length} style=${{ height: 1, padding: 0 }}>
+                <${LinearProgress} />
               </${TableCell}>
-            `))}
-          </${TableRow}>
-        `))}
-      </${TableHead}>
-      <${TableBody}>
-        ${rows.map((row) => {
-          prepareRow(row)
-          return html`
-            <${TableRow} ...${row.getRowProps(rowProps(row))}>
-              ${row.cells.map(cell => {
-                return html`
-                  <${TableCell} ...${cell.getCellProps()}>
-                    ${cell.render('Cell')}
-                  </${TableCell}>
-                `
-              })}
             </${TableRow}>
-          `
-        })}
-      </${TableBody}>
+          `}
+          ${headerGroups.map(headerGroup => (html`
+            <${TableRow} ...${headerGroup.getHeaderGroupProps()}>
+              ${headerGroup.headers.map(column => (html`
+                <${TableCell} ...${column.getHeaderProps()}>
+                  ${column.render('Header')}
+                </${TableCell}>
+              `))}
+            </${TableRow}>
+          `))}
+        </${TableHead}>
+        <${TableBody}>
+          ${rows.map((row) => {
+            prepareRow(row)
+            return html`
+              <${TableRow} ...${row.getRowProps(rowProps(row))}>
+                ${row.cells.map(cell => {
+                  return html`
+                    <${TableCell} ...${cell.getCellProps()}>
+                      ${cell.render('Cell')}
+                    </${TableCell}>
+                  `
+                })}
+              </${TableRow}>
+            `
+          })}
+        </${TableBody}>
       </${Table}>
+      ${fetchMore && html`
+        <div className=${classes.fetchMore}>
+          <${Chip}
+            color=primary
+            label='Fetch more rows'
+            clickable 
+            onClick=${fetchMore}
+          />
+        </div>
+      `}
     </${TableContainer}>
   `
 }

commit de217dd7d75ee8f404bd7565a0d0889b52e2b545

fix bug introduced by c267a81 (#21)

src/DataView/js/DataView.js
@@ -24,10 +24,24 @@ const client = new ApolloClient({
         fields: {
           querySlidesByStatus: {
             keyArgs: ["Status"],
-            merge(existing = { }, incoming) {
-              let mergedItems = [...existing.items || [], ...incoming.items];
-              return {...incoming, items: mergedItems};
+            merge(existing, incoming, { readField }) {
+              let items = existing ? { ...existing.items } : {};
+              incoming.items.forEach(item => {
+                items[readField("ImageID", item)] = item;
+              });
+              return {
+                nextToken: incoming.nextToken,
+                items,
+              };
             },
+            read(existing) {
+              if (existing) {
+                return {
+                  nextToken: existing.nextToken,
+                  items: Object.values(existing.items)
+                };
+              }
+            }
           }
         }
       }