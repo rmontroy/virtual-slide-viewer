8ba689a23029e79f5fd9c9be55164c2f3ca6ae40
rmontroy <rob.montroy@vai.org>
2021-01-21 15:31:00 -0500

    app-scripts-react cfg causes probs on newer vers

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 module.exports = {
-  "extends": "@snowpack/app-scripts-react",
   "plugins": [
+    '@snowpack/plugin-react-refresh', '@snowpack/plugin-babel', '@snowpack/plugin-dotenv',
     ["@snowpack/plugin-run-script", {
       "cmd": "eslint 'src/**/*.{js,jsx,ts,tsx}'",
       // Optional: Use npm package "watch" to run on every file change
